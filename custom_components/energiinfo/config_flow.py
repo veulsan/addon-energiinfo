6dbabd25a81b4838649057ca00571800483e6e05
Ulrik Sannsell.local <ulrik.sannsell@frejaeid.se>
2024-04-07 19:45:23 +0000

    Updated config flow

--- custom_components/energiinfo/config_flow.py
+++ custom_components/energiinfo/config_flow.py
@@ -12,6 +12,10 @@ from homeassistant.const import CONF_NAME, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
 from energiinfo.api import EnergiinfoClient
+from homeassistant.helpers import (
+    device_registry as dr,
+    entity_registry as er,
+)
 
 from .const import (
     DOMAIN,
@@ -20,6 +24,7 @@ from .const import (
     CONF_METERID,
     CONF_STORED_TOKEN,
     CONF_DAYS_BACK,
+    CONF_LAST_UPDATE,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -29,6 +34,14 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_URL): str,
         vol.Required(CONF_SITEID): str,
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
+        vol.Required(CONF_DAYS_BACK): int,
+    }
+)
+
+STEP_RECONF_DATA_SCHEMA = vol.Schema(
+    {
         vol.Required(CONF_USERNAME): str,
         vol.Required(CONF_PASSWORD): str,
         vol.Required(CONF_DAYS_BACK): int,
@@ -74,13 +87,13 @@ class EnergiinfoConfigFlow(ConfigFlow, domain=DOMAIN):
     async def authenticate(
         self, username: str, password: str
     ) -> tuple[bool, dict[str, Any]]:
-        """Get the QR code."""
+        """Authenticate"""
         token = await self.hass.async_add_executor_job(
             self.__api.authenticate, username, password
         )
         self.__token = token
         if self.__api.getStatus() == "ERR":
-            _LOGGER.info(self.__api.getErrorMessage)
+            _LOGGER.error(self.__api.getErrorMessage)
             raise InvalidAuth
         return self.__api.getStatus()
 
@@ -90,15 +103,6 @@ class EnergiinfoConfigFlow(ConfigFlow, domain=DOMAIN):
             self.__api.get_metering_points
         )
         return self.__api.getStatus(), meter_list
-
-    async def async_step_reconfigure(self, user_input: dict[str, Any] | None = None):
-        if user_input is not None:
-            pass  # TODO: process user input
-
-        return self.async_show_form(
-            step_id="reconfigure",
-            data_schema=vol.Schema({vol.Required("input_parameter"): str}),
-        )
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -144,15 +148,6 @@ class EnergiinfoConfigFlow(ConfigFlow, domain=DOMAIN):
         # User did not confirm, return to the previous step
         return self.async_show_form(step_id="confirm", data_schema=DATA_SCHEMA)
 
-    async def async_step_reconfigure(self, user_input: dict[str, Any] | None = None):
-        if user_input is not None:
-            pass  # TODO: process user input
-
-        return self.async_show_form(
-            step_id="reconfigure",
-            data_schema=vol.Schema({vol.Required("input_parameter"): str}),
-        )
-
     async def async_step_meter(self, user_input: Dict[str, Any] = None):
         """Handle meter selection."""
         # Extract meter information from input
@@ -183,6 +178,7 @@ class EnergiinfoConfigFlow(ConfigFlow, domain=DOMAIN):
                     CONF_USERNAME: self.__username,
                     CONF_PASSWORD: self.__password,
                     CONF_DAYS_BACK: self.__days_back,
+                    CONF_LAST_UPDATE: None,
                 },
             )
 
@@ -198,6 +194,62 @@ class EnergiinfoConfigFlow(ConfigFlow, domain=DOMAIN):
             ),
         )
 
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle a reconfiguration flow initialized by the user."""
+        self.config_entry = self.hass.config_entries.async_get_entry(
+            self.context["entry_id"]
+        )
+        return await self.async_step_reconfigure_confirm()
+
+    async def async_step_reconfigure_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle a reconfiguration flow initialized by the user."""
+        errors: dict[str, str] = {}
+        assert self.config_entry
+
+        if user_input is not None:
+            self.__api = EnergiinfoClient(
+                self.config_entry.data[CONF_URL], self.config_entry.data[CONF_SITEID]
+            )
+            try:
+                status = await self.authenticate(
+                    user_input[CONF_USERNAME], user_input[CONF_PASSWORD]
+                )
+                if status == "OK":
+                    user_input[CONF_STORED_TOKEN] = self.__token
+                    days_back: int = user_input[CONF_DAYS_BACK]
+                    old_days_back = self.config_entry.data[CONF_DAYS_BACK]
+                    _LOGGER.debug(
+                        f"Changed days_back from {old_days_back} to {days_back}"
+                    )
+                    # Update config_entry with data from user iser_input
+                    user_input = {**self.config_entry.data, **user_input}
+                    self.hass.config_entries.async_update_entry(
+                        self.config_entry, data=user_input
+                    )
+                    await self.hass.config_entries.async_reload(
+                        self.config_entry.entry_id
+                    )
+                    return self.async_abort(reason="reauth_successful")
+                else:
+                    errors["base"] = "invalid_auth"
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+        return self.async_show_form(
+            step_id="reconfigure_confirm",
+            data_schema=STEP_RECONF_DATA_SCHEMA,
+            errors=errors,
+        )
+
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
@@ -205,4 +257,3 @@ class CannotConnect(HomeAssistantError):
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
-
